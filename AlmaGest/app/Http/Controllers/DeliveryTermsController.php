852aa2bdd39a82f84b33b6c45b58ba23c85366f6
santogarrido <santocildesgarridoraul@gmail.com>
2025-10-23 13:18:47 +0200

    Creacion de tablas transports, payment_terms, bank_entity, delivery_terms

---
+++ AlmaGest/app/Http/Controllers/DeliveryTermsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Delivery_terms;
+use Illuminate\Http\Request;
+
+class DeliveryTermsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Delivery_terms $delivery_terms)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Delivery_terms $delivery_terms)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Delivery_terms $delivery_terms)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Delivery_terms $delivery_terms)
+    {
+        //
+    }
+}
