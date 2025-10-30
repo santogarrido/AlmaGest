ac8960a71a944734f31cec88dc6382bae88d4803
santogarrido <santocildesgarridoraul@gmail.com>
2025-10-30 09:42:35 +0100

    the others factories

--- AlmaGest/database/seeders/DatabaseSeeder.php
+++ AlmaGest/database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
         \App\Models\Company::factory(50)->create();
         \App\Models\User::factory(50)->create();
         \App\Models\User::factory(1)->create(['firstname' => 'Administrador', 'email' => 'admin@admin.com', 'password'=> '12345678', 'activated' => 1, 'email_confirmed' => 1]);
-
+        \App\Models\PaymentTerm::factory(50)->create();
+        \App\Models\BankEntity::factory(50)->create();
+        \App\Models\Discount::factory(50)->create();
     }
 }
