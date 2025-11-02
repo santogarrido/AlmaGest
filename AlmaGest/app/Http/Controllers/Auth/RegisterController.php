e00fad0d0ef6a2c75b1eda68fba458f1080b4c56
Flagn3 <flagn3@gmail.com>
2025-11-02 22:27:39 +0100

    Register Controller and view

--- AlmaGest/app/Http/Controllers/Auth/RegisterController.php
+++ AlmaGest/app/Http/Controllers/Auth/RegisterController.php
@@ -49,9 +49,11 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'firstname' => ['required', 'string', 'max:15'],
+            'secondname' => ['required', 'string', 'max:50'],
+            'email' => ['required', 'string', 'email', 'max:40', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'company_id' => ['required', 'exists:companies,id'],
         ]);
     }
 
@@ -64,9 +66,16 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'firstname' => $data['firstname'],
+            'secondname' => $data['secondname'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'password' => bcrypt($data['password']), //Hash::make($data['password']),
+            'company_id'=> $data['company_id'],
+            'type'=> 'U',
+            'email_confirmed' => 0,
+            'activated' => 0,
+            'iscontact' => 0,
+            'deleted' => 0
         ]);
     }
 }
