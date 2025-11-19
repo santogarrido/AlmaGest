8432fb011bc0daf8a5b727c2439dee7d16e0ffbb
santogarrido <santocildesgarridoraul@gmail.com>
2025-11-19 12:26:14 +0100

    Created store method in article controller

---
+++ AlmaGest/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return Article::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'price_min' => $request->price_min,
+            'price_max' => $request->price_max,
+            'color_name' => $request->color_name,
+            'weight' => $request->weight,
+            'size' => $request->size,
+            'family_id' => $request->family_id,
+        ]);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ AlmaGest/database/factories/ArticleFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'description' => fake()->text(150),
+            'price_min' => fake()->numberBetween(0, 9999999999),
+            'price_max' => fake()->numberBetween(0, 9999999999),
+            'color_name' => fake()->colorName(),
+            'weight' => fake()->numberBetween(0, 9999999999),
+            'size' => fake()->text(10),
+            'family_id' => App\Models\Family::all()->random()->id,
+        ];
+    }
+}
