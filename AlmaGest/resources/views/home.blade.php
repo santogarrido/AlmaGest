6eb954009ef3fef40f81c5ec08810ba4231f0297
santogarrido <santocildesgarridoraul@gmail.com>
2026-01-13 18:21:45 +0100

    fix merge

--- AlmaGest/resources/views/home.blade.php
+++ AlmaGest/resources/views/home.blade.php
@@ -2,19 +2,27 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row justify-content-center align-items-center" style="min-height: 80vh;">
+        <div class="col-md-6 col-lg-4">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-body text-center">
 
-                <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <div class="d-grid gap-3">
+                        <a href="{{ url('/empresa') }}" class="btn btn-primary">
+                            Datos de empresa
+                        </a>
+
+                        <a href="{{ url('/pedidos') }}" class="btn btn-primary">
+                            Pedidos
+                        </a>
+                    </div>
+
                 </div>
             </div>
         </div>
